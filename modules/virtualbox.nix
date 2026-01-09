6ac0906d490731d277efdc47170da9b92a0481a6
charlie <armstrong.charlie@proton.me>
2026-01-09 19:12:14 +0000

    remove virtualbox

--- modules/virtualbox.nix
+++
@@ -1,6 +0,0 @@
-{
-  nixpkgs.config.allowUnfree = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.users.charlie.extraGroups = ["vboxusers"];
-}
